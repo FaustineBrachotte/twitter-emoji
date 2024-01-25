4872e0dc0bd9ef184e3eff320b33724277adb685
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-01-25 14:59:46 +0100

    Postview component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
 import Head from "next/head";
 import Link from "next/link";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  console.log(user);
+
+  if (!user) return null
+
+  return <div className="flex gap-3 w-full">
+    <img src={user.profileImageUrl} alt="Profile image" className="h-14 w-14 rounded-full" />
+    <input placeholder="Type some emojis!" className="bg-transparent grow outline-none" />
+  </div>
+
+}
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex p-4 gap-3 border-b border-slate-400">
+      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full" />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300">
+          <span>{`@${author.username}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+
+    </div >
+  )
+}
 
 export default function Home() {
   const user = useUser();
@@ -23,10 +54,12 @@ export default function Home() {
         <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
           <div className="border-b border-slate-400 p-4 flex">
             {!user.isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
-            {!!user.isSignedIn && <SignOutButton />}
+            {user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {data?.map((post) => (<div key={post.id} className="p-8 border-b border-slate-400">{post.content}</div>))}
+            {data?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
+            ))}
           </div>
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
         </div>
